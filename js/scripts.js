4ff410edd50f7d1a1b14da5e0ad12f51f72eb903
Ubersp4m <109011348+Ubersp4m@users.noreply.github.com>
2025-12-01 17:22:29 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -64,33 +64,34 @@ function loadEmployees(employeesFilter){
     loads the modal with employee information using insertAdjacentHTML
 */
 function loadModal(employee){
-    const dobTimeObj = new Date(employee.dob.date);
-    const dobFormatted = dobTimeObj.toLocaleDateString();
-    //console.log(dobFormatted);
-    modalHTML = `
-                <div class="modal-container">
-                <div class="modal">
-                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                    <div class="modal-info-container">
-                        <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
-                        <h3 id="name" class="modal-name cap" data-fullname="${employee.name.first} ${employee.name.last}">${employee.name.first} ${employee.name.last}</h3>
-                        <p class="modal-text">${employee.email}</p>
-                        <p class="modal-text cap">${employee.location.city}</p>
-                        <hr>
-                        <p class="modal-text">${employee.phone}</p>
-                        <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
-                        <p class="modal-text">Birthday: ${dobFormatted}</p>
+    if(employee){
+        const dobTimeObj = new Date(employee.dob.date);
+        const dobFormatted = dobTimeObj.toLocaleDateString();
+        //console.log(dobFormatted);
+        modalHTML = `
+                    <div class="modal-container">
+                    <div class="modal">
+                        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                        <div class="modal-info-container">
+                            <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
+                            <h3 id="name" class="modal-name cap" data-fullname="${employee.name.first} ${employee.name.last}">${employee.name.first} ${employee.name.last}</h3>
+                            <p class="modal-text">${employee.email}</p>
+                            <p class="modal-text cap">${employee.location.city}</p>
+                            <hr>
+                            <p class="modal-text">${employee.phone}</p>
+                            <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
+                            <p class="modal-text">Birthday: ${dobFormatted}</p>
+                        </div>
+                    </div>
+    
+                    <div class="modal-btn-container">
+                        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                        <button type="button" id="modal-next" class="modal-next btn">Next</button>
                     </div>
                 </div>
- 
-                <div class="modal-btn-container">
-                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
-                </div>
-            </div>
-    `;   
-    document.body.insertAdjacentHTML('beforeend', modalHTML);
-
+        `;   
+        document.body.insertAdjacentHTML('beforeend', modalHTML);
+    }
 }
 /*
     Handles logic for searching employees by First and Last name
